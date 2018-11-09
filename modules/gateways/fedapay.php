291435285755c5e130db540a6974123d6777d147
Boris KOUMONDJI <brexis2009@yahoo.fr>
2018-11-09 10:45:47 +0100

    Comment file

--- modules/gateways/fedapay.php
+++ modules/gateways/fedapay.php
@@ -1,28 +1,15 @@
 <?php
 /**
- * WHMCS Sample Payment Gateway Module
- *
+ * FedaPay Payment Gateway Module
+ * Version 0.1.1
  * Payment Gateway modules allow you to integrate payment solutions with the
  * WHMCS platform.
- *
- * This sample file demonstrates how a payment gateway module for WHMCS should
- * be structured and all supported functionality it can contain.
- *
- * Within the module itself, all functions must be prefixed with the module
- * filename, followed by an underscore, and then the function name. For this
- * example file, the filename is "gatewaymodule" and therefore all functions
- * begin "fedapay_".
- *
- * If your module or third party API does not support a given function, you
- * should not define that function within your module. Only the _config
- * function is required.
- *
  * For more information, please refer to the online documentation.
  *
  * @see https://developers.whmcs.com/payment-gateways/
  *
- * @copyright Copyright (c) WHMCS Limited 2017
- * @license http://www.whmcs.com/license/ WHMCS Eula
+ * @copyright Copyright (c) FedaPay 2018
+ * @license https://www.fedapay.com/license/
  */
 
 if (!defined("WHMCS")) {
